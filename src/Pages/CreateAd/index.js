0aded7bd187be963b070e08db54a222a016b2cec
MillenaQueiroz <millena.queiroz08@gmail.com>
2021-10-19 18:16:07 -0300

    Add inputs

--- src/Pages/CreateAd/index.js
+++ src/Pages/CreateAd/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
+import styles from './styles';
 
 const CreateAd = () => {
+  const [number, onChangeNumber] = React.useState(null);
+  const [descriptionNumber, onChangeDescriptionNumber] = React.useState(null);
   return (
     <View>
-      <Text>Hello World</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Título do anúncio*</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Ex: Samsung S9 novo na caixa"
+          keyboardType="text"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Descrição*</Text>
+        <TextInput
+          style={styles.inputDescription}
+          onChangeText={onChangeDescriptionNumber}
+          value={descriptionNumber}
+          placeholder="Ex: Samsung S9 novo na caixa com 128gb de memória, com caixa, todos os cabos e sem marcas de uso."
+          keyboardType="text"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Categoria*</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Ex: Samsung S9 novo na caixa"
+          keyboardType="text"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>CEP*</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Ex: Samsung S9 novo na caixa"
+          keyboardType="text"
+        />
+      </View>
     </View>
   );
 };
---
+++ src/Pages/CreateAd/styles.js
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  inputText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    marginTop: 50,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  input: {
+    height: 40,
+    marginTop: 10,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8,
+  },
+  inputDescription: {
+    height: 120,
+    marginTop: 10,
+    borderWidth: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 8,
+    textAlign: 'justify',
+  },
+  inputPicker:{
+    height: 50,
+    width: 150,
+  },
+});
+
+export default styles;
