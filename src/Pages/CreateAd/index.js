9d3b372f1354d21a69a637ec18338428124835be
MillenaQueiroz <millena.queiroz08@gmail.com>
2021-10-21 00:10:46 -0300

    add the checkbox area

--- src/Pages/CreateAd/index.js
+++ src/Pages/CreateAd/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { View, Text, TextInput, Image, ScrollView } from 'react-native';
-import CheckBox from 'react-native-check-box';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  ScrollView,
+  Pressable,
+} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 import styles from './styles';
 
 const CreateAd = () => {
@@ -8,64 +15,105 @@ const CreateAd = () => {
   const [descriptionNumber, onChangeDescriptionNumber] = React.useState(null);
   const [cepNumber, onChangeCepNumber] = React.useState(null);
   const [category, onChangeCategory] = React.useState(null);
-  const [toggleCheckBox, setToggleCheckBox] = React.useState(false);
+  const [agree, setAgree] = React.useState(false);
+  const [agreeGreenSpace, setAgreeGreenSpace] = React.useState(false);
 
   return (
-    <View>
-      <ScrollView>
-        <Image
-          style={styles.photoImage}
-          source={require('../../assets/incluir-fotos.jpeg')}
-        />
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputText}>Título do anúncio*</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangeNumber}
-            value={number}
-            placeholder="Ex: Samsung S9 novo na caixa"
-            keyboardType="text"
+    <>
+      <View>
+        <ScrollView>
+          <Image
+            style={styles.photoImage}
+            source={require('../../assets/incluir-fotos.jpeg')}
           />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputText}>Descrição*</Text>
-          <TextInput
-            style={styles.inputDescription}
-            onChangeText={onChangeDescriptionNumber}
-            value={descriptionNumber}
-            multiline={true}
-            placeholder="Ex: Samsung S9 novo na caixa com 128gb de memória, com caixa, todos os cabos e sem marcas de uso."
-            keyboardType="text"
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputText}>Categoria*</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangeCategory}
-            value={category}
-            placeholder="Ex: Móveis"
-            keyboardType="text"
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputText}>CEP*</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangeCepNumber}
-            value={cepNumber}
-            keyboardType="numeric"
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          {/* <CheckBox
-            disabled={false}
-            value={toggleCheckBox}
-            onValueChange={(newValue) => setToggleCheckBox(newValue)}
-          /> */}
-        </View>
-      </ScrollView>
-    </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Título do anúncio*</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeNumber}
+              value={number}
+              placeholder="Ex: Samsung S9 novo na caixa"
+              keyboardType="text"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Descrição*</Text>
+            <TextInput
+              style={styles.inputDescription}
+              onChangeText={onChangeDescriptionNumber}
+              value={descriptionNumber}
+              multiline={true}
+              placeholder="Ex: Samsung S9 novo na caixa com 128gb de memória, com caixa, todos os cabos e sem marcas de uso."
+              keyboardType="text"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Categoria*</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeCategory}
+              value={category}
+              placeholder="Ex: Móveis"
+              keyboardType="text"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>CEP*</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeCepNumber}
+              value={cepNumber}
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.checkboxContainer}>
+            <CheckBox
+              value={agree}
+              tintColors={{true: '#613D97', false: 'black'}}
+              onChange={() => setAgree(!agree)}
+            />
+            <Text style={styles.checkboxText}>Compra Segura</Text>
+          </View>
+          {agree ? (
+            <>
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                  value={agreeGreenSpace}
+                  onChange={() => setAgreeGreenSpace(!agreeGreenSpace)}
+                  tintColors={{true: '#613D97', false: 'black'}}
+                />
+                <Text style={styles.greenCheckboxText}>
+                  Eu gostaria de usar um Posto de Troca
+                </Text>
+                <Image
+                  style={styles.leafImage}
+                  source={require('../../assets/leaf.png')}
+                />
+              </View>
+              {agreeGreenSpace ? (
+                <>
+                  <Image
+                    style={styles.mapsImage}
+                    source={require('../../assets/maps.png')}
+                  />
+                </>
+              ) : (
+                <View style={styles.space} />
+              )}
+            </>
+          ) : (
+            <View style={styles.space} />
+          )}
+          <View style={styles.button}>
+            <Pressable
+              style={styles.CreateAd}
+              onPress={() => console.log('produto cadastrado')}>
+              <Text style={styles.buttonText}>Enviar anúncio</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </View>
+    </>
   );
 };
 
--- src/Pages/CreateAd/styles.js
+++ src/Pages/CreateAd/styles.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   inputText: {
@@ -34,7 +34,58 @@ const styles = StyleSheet.create({
     width: 'auto',
     height: 295,
   },
-  checkbox: {
+  mapsImage: {
+    marginTop: '5%',
+    marginBottom: '5%',
+    width: 'auto',
+    height: 250,
+  },
+  checkboxText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: '10%',
+    marginLeft: '9%',
+  },
+  greenCheckboxText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#86D940',
+    position: 'absolute',
+    top: '10%',
+    marginLeft: '9%',
+  },
+  checkboxContainer: {
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  button: {
+    width: '100%',
+    marginBottom: '5%',
+  },
+  CreateAd: {
+    width: '90%',
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 20,
+    backgroundColor: '#F88324',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 17,
+    color: '#fff',
+  },
+  space: {
+    marginBottom: '7%',
+  },
+  leafImage: {
+    position: 'absolute',
+    marginLeft: 335,
     alignSelf: 'center',
   },
 });
